2079bae40cf8af2c6401e5318516a0306e07abd5
tania <tanislavens@yahoo.com>
2019-06-25 14:02:18 -0400

    WIP: restructured home header. menu still not working

--- js/index.js
+++ js/index.js
@@ -1,18 +1,21 @@
 let toggleMenu = () => {
     //toggle menu
-    menu.classList.toggle('menu-selected');
+    menu.classList.toggle('menu--open');
+    console.log(toggleMenu);
 }
 
 //reference menu
 const menu = document.querySelector('.menu');
+console.log(menu);
 
 //reference menu Button
-const menuOpen = document.querySelector('.menu-button');
-menuOpen.addEventListener('click', toggleMenu);
+const menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', toggleMenu);
+console.log(menuButton);
 
 //reference menu Button close
 
 const menuClose = document.querySelector('.menu-button-close');
 menuClose.addEventListener('click', toggleMenu);
-
+console.log(menuClose);
 
